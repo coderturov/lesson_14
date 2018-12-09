79d527e8e246cdb0b473661a1e2d9f9d1c1b49dd
Vladimir <vladimir_t1@mail.ru>
2018-12-09 18:08:37 +0500

    готово

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('nav li:eq(1),.main_btna,.main_btn').on('click', function() {
+        $('.overlay').fadeIn(1000);
+        $('.modal').slideDown(1000);
+    });
+    $('.close').on('click', function() {
+        $('.overlay').fadeOut(1000);
+        $('.modal').slideUp(1000);
+    });
+});
+
